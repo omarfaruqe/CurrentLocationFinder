092613cded4f5e05bac0b6208a87268a5e055e75
Omar Faruqe <faruqe.cse@gmail.com>
2016-02-28 14:17:13 -0500

    Initial Commit

---
+++ CurrentLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrentLocation
+//
+//  Created by Omar Faruqe on 2016-02-28.
+//  Copyright © 2016 Omar Faruqe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
