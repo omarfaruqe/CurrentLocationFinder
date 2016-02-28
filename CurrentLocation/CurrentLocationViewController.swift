88eb296105fcc74729be0d3ffb7da048b4f2b0cf
Omar Faruqe <faruqe.cse@gmail.com>
2016-02-28 14:40:46 -0500

    Current location finder

---
+++ CurrentLocation/CurrentLocationViewController.swift
@@ -0,0 +1,70 @@
+//  CurrentLocationViewController.swift
+//  Created by Omar Faruqe on 2016-02-28.
+//  Copyright © 2016 Omar Faruqe. All rights reserved.
+
+import UIKit
+import CoreLocation
+
+
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var currentLocation: UIBarButtonItem!
+        let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    @IBAction func reloadAction(sender: UIBarButtonItem) {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error)->Void in
+            
+            if (error != nil) {
+                print("Reverse geocoder failed with error" + error!.localizedDescription)
+                return
+            }
+            
+            if placemarks!.count > 0 {
+                let pm = placemarks![0]
+                self.displayLocationInfo(pm)
+            } else {
+                print("Problem with the data received from geocoder")
+            }
+        })
+    }
+    
+    func displayLocationInfo(placemark: CLPlacemark?) {
+        if let containsPlacemark = placemark {
+            //stop updating location to save battery life
+            locationManager.stopUpdatingLocation()
+            let locality = (containsPlacemark.locality != nil) ? containsPlacemark.locality : ""
+            let postalCode = (containsPlacemark.postalCode != nil) ? containsPlacemark.postalCode : ""
+            let administrativeArea = (containsPlacemark.administrativeArea != nil) ? containsPlacemark.administrativeArea : ""
+            let country = (containsPlacemark.country != nil) ? containsPlacemark.country : ""
+            
+            currentLocation.title = "\(locality!), \(postalCode!), \(administrativeArea!), \(country!)"
+//            localityTxtField.text = locality
+//            postalCodeTxtField.text = postalCode
+//            aAreaTxtField.text = administrativeArea
+//            countryTxtField.text = country
+        }
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error while updating location " + error.localizedDescription)
+    }
+}
